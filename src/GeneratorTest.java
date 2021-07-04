8a0c03a78f79230dea1260c2e706a83156598bfe
Koray1999 <77972012+Koray1999@users.noreply.github.com>
2021-07-04 16:08:50 +0200

    Methode für Abhängigkeiten hinzugefügt

--- src/GeneratorTest.java
+++ src/GeneratorTest.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class GeneratorTest {
@@ -39,6 +40,8 @@ public class GeneratorTest {
             konfiguration.add(gerätErstellen());
         }
         System.out.println(konfiguration);
+
+        abhängigkeitenErstellen2(konfiguration);
     }
 
     //Methode zum Geräte generieren
@@ -58,16 +61,62 @@ public class GeneratorTest {
 
         //Leere Updates mit Dienstleistungen füllen
         for(int i=0; i<updates.size();i++){
-            for(int j=0; updates.get(i).size()<anzahlDiensleistungen; j++){
+            while(updates.get(i).size()<anzahlDiensleistungen){
                 hilfsVariable = rand.nextInt(10);
                 if(!updates.get(i).contains(hilfsVariable)){
                     updates.get(i).add(hilfsVariable);
                 }
             }
+
         }
         //Leere Updates mit Dienstleistungen füllen
 
         Device device = new Device(versionsNummer, updates.get(0), updates);
         return device;
     }
+    //Methode zum Geräte generieren
+
+    //Methode zum Abhängigkeiten generieren
+    public static void abhängigkeitenErstellen(ArrayList<Device> konfiguration){
+        Random rand = new Random();
+        ArrayList<ArrayList<Integer>> abhängigkeiten = new ArrayList<ArrayList<Integer>>();
+        int anzahlGeräte = konfiguration.size();
+
+        for(int i=0; i<anzahlGeräte; i++){
+            abhängigkeiten.add(new ArrayList<Integer>());
+        }
+
+        for(int i=0; i<anzahlGeräte; i++){
+            ArrayList<Integer> hilfsArray = konfiguration.get(i).getDienstleistungen();
+            int hilfVariable = hilfsArray.get(rand.nextInt(10));
+
+            ArrayList<Integer> test = new ArrayList<Integer>();
+            test.add(i);
+            test.add(hilfsArray.get(hilfVariable));
+            test.add(rand.nextInt(10));
+        }
+    }
+    //Methode zum Abhängigkeiten generieren
+
+    //Methode zum Abhängigkeiten generieren
+    public static void abhängigkeitenErstellen2(ArrayList<Device> konfiguration){
+        ArrayList<ArrayList<Integer>> abhängigkeiten = new ArrayList<ArrayList<Integer>>();
+        int anzahlGeräte = konfiguration.size();
+        ArrayList<Integer> momentanteDienstleistungen = new ArrayList<Integer>();
+        Random rand = new Random();
+
+        for(int i=0; i<anzahlGeräte; i++){
+            abhängigkeiten.add(new ArrayList<Integer>());
+        }
+
+        for(int i=0; i<anzahlGeräte; i++){
+            Device momentantesGerät = konfiguration.get(i);
+            momentanteDienstleistungen = momentantesGerät.getDienstleistungen();
+            abhängigkeiten.get(i).add(i);
+            abhängigkeiten.get(i).add(momentanteDienstleistungen.get(rand.nextInt(10)));
+            abhängigkeiten.get(i).add(rand.nextInt(9)+1);
+        }
+        System.out.println(abhängigkeiten);
+    }
+    //Methode zum Abhängigkeiten generieren
 }
