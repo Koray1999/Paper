39e63a0e03806425a4476d66671d63dde6bb14ae
Koray1999 <77972012+Koray1999@users.noreply.github.com>
2021-07-05 15:11:30 +0200

    SmartHome hinzugefügt

---
+++ src/GeneratorFinal.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GeneratorFinal {
+
+    public static void main (String[] args){
+        ArrayList<Device> konfiguration = new ArrayList<Device>();
+
+        for(int i=0; i<10; i++){
+            konfiguration.add(gerätErstellen(3,10));
+        }
+        System.out.println(konfiguration);
+
+        abhängigkeitenErstellen(konfiguration);
+    }
+
+    //Methode zum Geräte generieren
+    public static Device gerätErstellen(int nrOfUpdatesPerDevice, int nrOfServicesPerDevice){
+        Random rand = new Random();
+        int anzahlUpdatesProGerät = nrOfUpdatesPerDevice;
+        int versionsNummer = rand.nextInt(nrOfUpdatesPerDevice);
+        int anzahlDiensleistungen = nrOfServicesPerDevice;
+        int hilfsVariable;
+        ArrayList<ArrayList<Integer>> updates = new ArrayList<ArrayList<Integer>>();
+
+        //Leere Updates in Liste eintragen
+        for(int i=0; i<anzahlUpdatesProGerät; i++){
+            updates.add(new ArrayList<Integer>());
+        }
+        //Leere Updates in Liste eintragen
+
+        //Leere Updates mit Dienstleistungen füllen
+        for(int i=0; i<updates.size();i++){
+            while(updates.get(i).size()<anzahlDiensleistungen){
+                hilfsVariable = rand.nextInt(10);
+                if(!updates.get(i).contains(hilfsVariable)){
+                    updates.get(i).add(hilfsVariable);
+                }
+            }
+
+        }
+        //Leere Updates mit Dienstleistungen füllen
+
+        Device device = new Device(versionsNummer, updates.get(0), updates);
+        return device;
+    }
+    //Methode zum Geräte generieren
+
+    //Methode zum Abhängigkeiten generieren
+    public static void abhängigkeitenErstellen(ArrayList<Device> konfiguration){
+        ArrayList<ArrayList<Integer>> abhängigkeiten = new ArrayList<ArrayList<Integer>>();
+        int anzahlGeräte = konfiguration.size();
+        ArrayList<Integer> momentanteDienstleistungen = new ArrayList<Integer>();
+        Random rand = new Random();
+
+        for(int i=0; i<anzahlGeräte; i++){
+            abhängigkeiten.add(new ArrayList<Integer>());
+        }
+
+        for(int i=0; i<anzahlGeräte; i++){
+            Device momentantesGerät = konfiguration.get(i);
+            momentanteDienstleistungen = momentantesGerät.getDienstleistungen();
+            abhängigkeiten.get(i).add(i);
+            abhängigkeiten.get(i).add(momentanteDienstleistungen.get(rand.nextInt(10)));
+            //Problem
+            int test = rand.nextInt(10);
+            if(test != i){
+                abhängigkeiten.get(i).add(test);
+            }else if(test < 9){
+                abhängigkeiten.get(i).add(test+1);
+            }else{
+                abhängigkeiten.get(i).add(test-1);
+            }
+
+        }
+        System.out.println(abhängigkeiten);
+    }
+    //Methode zum Abhängigkeiten generieren
+}
--- src/SmartHome.java
+++ src/SmartHome.java
@@ -1,3 +1,16 @@
-public class SmartHome {
+import java.util.ArrayList;
 
+public class SmartHome extends GeneratorFinal{
+    public static void main (String[] args){
+        ArrayList<Device> SmartHomeGeräte = new ArrayList<Device>();
+        int nrOfDevices = 10;
+        int nrOfUpdatesPerDevice = 3;
+        int nrOfServicesPerDevice = 10;
+
+        for(int i=0; i<nrOfDevices; i++){
+            SmartHomeGeräte.add(gerätErstellen(nrOfUpdatesPerDevice, nrOfServicesPerDevice));
+        }
+        System.out.println(SmartHomeGeräte);
+        abhängigkeitenErstellen(SmartHomeGeräte);
+    }
 }
